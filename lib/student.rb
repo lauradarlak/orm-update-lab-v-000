1da4b4cbda403ba5fa6eabe85588b0f81a3b6dbc
Laura Darlak <laura@polyporedesign.com>
2018-10-09 16:04:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade)
+  def initialize(name, grade, id)
     @name = name
     @grade = grade
     @id = nil
