109a43dc1621fd8c298ed1b964f0ed9bff92d80e
Laura Darlak <laura@polyporedesign.com>
2018-10-09 16:03:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
 
   def self.new_from_db(row)
     # ​convert what the database provides into a Ruby object
-    new_student = Student.new
+    new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
