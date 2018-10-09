e86528f85fcbead227abdab76e7977790919ed0f
Laura Darlak <laura@polyporedesign.com>
2018-10-09 16:09:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,4 +57,17 @@ class Student
     new_student # return the new student
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
